9dc0103ecbf0735cfe3089680cd3ff866a87beb6
LionAlpha <gabrielvesz_@hotmail.com>
2025-10-09 20:19:48 -0300

    Update InstagramFloat.tsx

--- src/components/InstagramFloat.tsx
+++ src/components/InstagramFloat.tsx
@@ -11,7 +11,8 @@ const InstagramFloat = () => (
       p-4 shadow-yellow 
       bg-gradient-to-tr from-[#feda75] via-[#d62976] to-[#962fbf]
       hover:scale-110 transition-transform duration-300 
-      flex items-center justify-center
+      flex items-center justify-center 
+      animate-float-in
     "
     aria-label="Abrir Instagram Full Force"
   >
