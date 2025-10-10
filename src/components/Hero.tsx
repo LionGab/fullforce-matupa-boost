be4b142640217aae802ecd83ee3755d09fee6715
LionAlpha <gabrielvesz_@hotmail.com>
2025-10-10 02:24:35 -0300

    Adjust padding in Hero component
    Increased bottom padding of the Hero section.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   };
 
   return (
-   <section className="relative min-h-[85vh] md:min-h-screen flex items-center justify-center pt-28 pb-10 overflow-hidden">
+   <section className="relative min-h-[85vh] md:min-h-screen flex items-center justify-center pt-28 pb-32 overflow-hidden">
       {/* Background Image with Overlay */}
       <div className="absolute inset-0 z-0">
         <img 
